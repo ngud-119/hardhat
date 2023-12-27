c539c837edd36ed6d9616613abe765b42529ce14
ngud-119 <passiondev7@gmail.com>
2023-12-27 04:18:35 -0500

    compiling contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,15 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.23",
+  solidity: {
+    version: "0.8.23",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
 };
 
 export default config;
