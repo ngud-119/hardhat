d8ef13eed7e59de82fac8f08efd3391466ce7e6d
ngud-119 <passiondev7@gmail.com>
2024-01-03 01:42:40 -0500

    writing-tasks

---
+++ scripts/accounts.js
@@ -0,0 +1,12 @@
+async function main() {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
